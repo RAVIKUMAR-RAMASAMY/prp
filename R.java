877c8ad222dc55121bdd88152f950ee94f05a7d8
RAVIKUMAR-RAMASAMY <43012391+RAVIKUMAR-RAMASAMY@users.noreply.github.com>
2019-08-13 18:37:05 +0530

    Create R.java

---
+++ R.java
@@ -0,0 +1,37 @@
+//import java.util.Scanner;
+import java.lang.*;
+public class R{
+public static void main(String args[])
+{
+//Scanner sc=new Scanner(System.in); 
+int inp1=6928;
+//int inp=sc.nextInt();
+int sub=0;
+int sum=0;
+int inp=0;
+while(inp1!=0)
+{
+  inp=inp*10+(inp1%10);
+  inp1/=10;
+}
+
+int x=inp;
+
+while(inp>99)
+{
+  sub=0;
+  sum=0;
+  x=inp;
+  while(x!=0)
+  {  
+      s
+     x/=10;
+    sum=(sum*10)+sub);
+   
+  }
+  inp=sum;
+}
+
+
+}
+}
